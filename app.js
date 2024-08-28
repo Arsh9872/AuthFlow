831db9c30047037c4a05cd847a5628c782d43a05
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-08-28 18:07:52 +0530

    authentication based upon custom users

--- app.js
+++ app.js
@@ -1,18 +1,66 @@
 import bcrypt from "bcrypt";
+import express from "express";
 
-const password = "12345";
+const app = express();
 
-const salt = await bcrypt.genSalt();
-console.log(salt);
+//middleware
+app.use(express.json());
 
+const users = [];
+app.post("/signup",async (req,res)=>{
+    //get the username and password
+    const {username,password}= req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.push({
+        username,
+        password : hashedPassword,
+    });
+    res.send("User created");
+});
+
+app.post("/login",async (req,res)=>{
+    const {username,password} = req.body;
+
+    //check username
+    const user = users.find((user) => user.username === username);
+    if(!user){
+        res.status(200).send("User not found");
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if(!isValid){
+        res.status(200).send("Incorrect Password");
+        return
+    }
+    res.send(`Welcome ${username}`);
+
+});
+
+
+app.listen(3000,()=>{
+    console.log(`server running on port 3000`);
+    
+})
+
+
+
+
+
+// -------------------------------------------------- Basics of app authentication ---------------------------------------------
+// const password = "12345";
+
+// const salt = await bcrypt.genSalt();
+// console.log(salt);
+
+// // const hashedPassword = await bcrypt.hash(password,10);
+// // console.log(hashedPassword);
+
+// console.time("hash");
 // const hashedPassword = await bcrypt.hash(password,10);
 // console.log(hashedPassword);
+// console.timeEnd("hash");
 
-console.time("hash");
-const hashedPassword = await bcrypt.hash(password,10);
-console.log(hashedPassword);
-console.timeEnd("hash");
+// //check the validity of password
+// const isValid = await bcrypt.compare("12345",hashedPassword);
+// console.log(isValid);
 
-//check the validity of password
-const isValid = await bcrypt.compare("12345",hashedPassword);
-console.log(isValid);
